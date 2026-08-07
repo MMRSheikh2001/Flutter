d2e3c3bc7e9c854f43fe1a1b31f634740529e7eb
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-08-08 00:36:58 +0600

    ert

--- homeflutterproject/lib/page/registration_prac.dart
+++ homeflutterproject/lib/page/registration_prac.dart
@@ -1,7 +1,33 @@
+import 'package:date_field/date_field.dart';
 import 'package:flutter/material.dart';
 
-class RegistrationPrac extends StatelessWidget {
+class RegistrationPrac extends StatefulWidget {
   const RegistrationPrac({super.key});
+
+  @override
+  State<RegistrationPrac> createState() => _RegistrationPracState();
+}
+
+class _RegistrationPracState extends State<RegistrationPrac> {
+
+  final TextEditingController name=TextEditingController();
+  final TextEditingController email=TextEditingController();
+  final TextEditingController password=TextEditingController();
+  final TextEditingController confirmPassword=TextEditingController();
+  final TextEditingController cell=TextEditingController();
+  final TextEditingController address=TextEditingController();
+
+final DateTimeFieldPickerPlatform dob=DateTimeFieldPickerPlatform.material;
+
+String? selectedGender;
+DateTime? selectedDate;
+
+final _formKey=GlobalKey<FormState>();
+
+
+
+
+
 
   @override
   Widget build(BuildContext context) {
