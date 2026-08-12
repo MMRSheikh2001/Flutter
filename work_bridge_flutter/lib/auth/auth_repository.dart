191aa4ece9c996d79ba9afa75483ec258e493f52
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-08-13 03:39:59 +0600

    add

---
+++ work_bridge_flutter/lib/auth/auth_repository.dart
@@ -0,0 +1,53 @@
+
+import 'package:dio/dio.dart';
+import 'package:work_bridge_flutter/auth/request/login_request.dart';
+import 'package:work_bridge_flutter/auth/response/login_response.dart';
+
+class AuthRepository{
+
+  AuthRepository(this._apiClient, this._storageService);
+
+  final ApiClient _apiClient;
+  final StorageService _storageService;
+
+  Dio get _dio => _apiClient.dio;
+
+  Future<LoginResponse> login(LoginRequest dto) async {
+    final res = await _dio.post(ApiConstants.login, data: dto.toJson());
+    final loginRes = LoginResponse.fromJson(res.data as Map<String, dynamic>);
+    await _storageService.saveSession(loginRes);
+    return loginRes;
+  }
+
+  Future<void> logout() => _storageService.clearSession();
+
+
+  Future<String> forgotPassword(ForgotPasswordRequest dto) async {
+    final res = await _dio.post(
+      ApiConstants.forgotPassword,
+      data: dto.toJson(),
+      options: Options(responseType: ResponseType.plain),
+    );
+    return res.data.toString();
+  }
+
+  Future<String> resetPassword(ResetPasswordRequest dto) async {
+    final res = await _dio.post(
+      ApiConstants.resetPassword,
+      data: dto.toJson(),
+      options: Options(responseType: ResponseType.plain),
+    );
+    return res.data.toString();
+  }
+
+  Future<String> verifyEmail(String token) async {
+    final res = await _dio.get(
+      ApiConstants.verifyEmail,
+      queryParameters: {'token': token},
+      options: Options(responseType: ResponseType.plain),
+    );
+    return res.data.toString();
+  }
+
+
+}
