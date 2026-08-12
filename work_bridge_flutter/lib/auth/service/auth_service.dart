47d4b515a3b85d42452e294249447f9303384b3a
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-08-12 18:21:23 +0600

    add

--- work_bridge_flutter/lib/auth/service/auth_service.dart
+++
@@ -1,9 +0,0 @@
-class AuthService {
-  final String baseUrl = "http://localhost:8090/api/auth";
-
-  Future<bool> login(String email, String password) async {
-    final url = Uri.parse('$baseUrl/login');
-
-    return true;
-  }
-}
