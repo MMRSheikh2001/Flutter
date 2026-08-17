dbeaeb4a4b5f90954fc46b9859fdeea3127f8431
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-08-17 11:59:17 +0600

    add

--- homeflutterproject/lib/prac/container_praca.dart
+++ homeflutterproject/lib/prac/container_praca.dart
@@ -1,9 +1,17 @@
 
 
+import 'dart:convert';
+import 'dart:ui_web';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class BigContainerPrac extends StatelessWidget {
-  const BigContainerPrac({super.key});
+  List<dynamic> user=[];
+
+   BigContainerPrac({super.key,required this.user});
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +24,21 @@ class BigContainerPrac extends StatelessWidget {
           color: Colors.red,
 
         ),
-      )
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: fetchRandomUsers),
     );
   }
+  void fetchRandomUsers() async{
+   final response=await   http.get(Uri.parse("https://randomuser.me/api/?results=5000"));
+
+   final body=response.body;
+
+   final json=jsonDecode(body);
+
+
+
+
+  }
+
+
 }
