ae1358a60ecc428aab4513d8a3f536baf23f1214
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-08-21 03:53:31 +0600

    add

--- work_bridge_flutter/lib/utils/providers.dart
+++ work_bridge_flutter/lib/utils/providers.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:work_bridge_flutter/auth/providers.dart';
 import 'package:work_bridge_flutter/chat/data/notification_repository.dart';
+import 'package:work_bridge_flutter/cvinformations/data/company_profile_repository.dart';
+import 'package:work_bridge_flutter/cvinformations/data/cv_repository.dart';
 import 'package:work_bridge_flutter/job/data/job_repository.dart';
 import 'package:work_bridge_flutter/masterdata/data/master_data_repository.dart';
 
@@ -14,4 +16,12 @@ final notificationRepositoryProvider = Provider<NotificationRepository>((ref) {
 
 final masterDataRepositoryProvider = Provider<MasterDataRepository>((ref) {
   return MasterDataRepository(ref.watch(apiClientProvider));
+});
+
+final cvRepositoryProvider = Provider<CvRepository>((ref) {
+  return CvRepository(ref.watch(apiClientProvider));
+});
+
+final companyProfileRepositoryProvider = Provider<CompanyProfileRepository>((ref) {
+  return CompanyProfileRepository(ref.watch(apiClientProvider));
 });
