6ba0c066e25e90e71ca317e81e59c27ff9239732
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-08-23 04:02:56 +0600

    add

---
+++ work_bridge_flutter/lib/cvinformations/screens/add_edit_education_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class AddEditEducationScreen extends StatefulWidget {
+  final int educationId ;
+
+  const AddEditEducationScreen({super.key,required this.educationId});
+
+  @override
+  State<AddEditEducationScreen> createState() => _AddEditEducationScreenState();
+}
+
+class _AddEditEducationScreenState extends State<AddEditEducationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
