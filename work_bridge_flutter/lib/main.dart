d808e5c47d598a2eaeaca8554c5eeee9ee7619f5
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-08-25 00:04:12 +0600

    add

--- work_bridge_flutter/lib/main.dart
+++ work_bridge_flutter/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:work_bridge_flutter/router/app_router.dart';
 import 'package:work_bridge_flutter/themes/app_theme.dart';
+import 'package:work_bridge_flutter/utils/navigation_service.dart';
 import 'package:work_bridge_flutter/utils/providers.dart';
 
 void main() {
@@ -19,6 +20,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'WorkBridge',
       theme: AppTheme.light_,
+      navigatorKey: NavigationService.navigatorKey,
       initialRoute: AppRouter.root,
       onGenerateRoute: AppRouter.onGenerateRoute,
     );
